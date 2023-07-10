6f446fbbb34b2929fb01b42fc09df5d6b14f2d33
MeParthDegama <hello@parthka.com>
2023-07-10 13:54:17 +0530

    add zigstr library

--- build.zig
+++ build.zig
@@ -4,6 +4,12 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    // zigstr library
+    const zigstr = b.addModule("zigstr", .{
+        .source_file = .{ .path = "./lib/zigstr/lib/str.zig" },
+    });
+
+    // hasm executable
     const exe = b.addExecutable(.{
         .name = "hasm",
         .root_source_file = .{ .path = "./src/hasm.zig" },
@@ -11,6 +17,9 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    exe.addModule("zigstr", zigstr);
+
+    // install
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -19,6 +28,7 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
+    // run command
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 }
--- src/hasm.zig
+++ src/hasm.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
+const String = @import("zigstr").String;
 const x86_64 = @import("./x86_64/x86_64.zig");
 
-pub fn main() !void {
-    
-    x86_64.print_info();
 
+pub fn main() !void {    
+    x86_64.print_info();
+    _ = String;
 }
