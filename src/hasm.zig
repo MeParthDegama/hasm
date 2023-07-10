988f8abe54bf8bc80c14f4c45c5ef3adb90a7e71
MeParthDegama <hello@parthka.com>
2023-07-10 22:19:06 +0530

    add arg info

--- src/hasm.zig
+++ src/hasm.zig
@@ -3,14 +3,34 @@ const String = @import("zigstr").String;
 const x86_64 = @import("./x86_64/x86_64.zig");
 
 pub fn main() !void {
-    x86_64.print_info();
-    try parseArg();
+    var args = try getArgs();
+
+    if (args.len < 2) {
+        std.debug.print("input file error...\n", .{});
+        return;
+    }
+
+    var arg1 = args[1];
+
+    if (arg1.equString("info")) {
+        x86_64.print_info();
+    } else {
+        std.debug.print("TODO...\n", .{});
+    }
 }
 
-fn parseArg() !void {
-    var a = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer a.deinit();
-
+fn getArgs() ![]String {
     var args = try std.process.argsAlloc(std.heap.page_allocator);
-    std.debug.print("{s} {}", .{args, args.len});
+
+    var stringA = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+
+    var stringAllocater = stringA.allocator();
+
+    var stringSpace = stringAllocater.alloc(String, args.len) catch unreachable;
+
+    for (args, 0..) |arg, i| {
+        stringSpace[i] = String.initString(arg);
+    }
+
+    return stringSpace;
 }
