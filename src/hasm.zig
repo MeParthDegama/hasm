cf2b71f5b0ebb34ec193678c4df3ca5e9a755a74
MeParthDegama <hello@parthka.com>
2023-07-10 14:12:36 +0530

    add arg parser

--- src/hasm.zig
+++ src/hasm.zig
@@ -2,8 +2,15 @@ const std = @import("std");
 const String = @import("zigstr").String;
 const x86_64 = @import("./x86_64/x86_64.zig");
 
-
-pub fn main() !void {    
+pub fn main() !void {
     x86_64.print_info();
-    _ = String;
+    try parseArg();
+}
+
+fn parseArg() !void {
+    var a = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer a.deinit();
+
+    var args = try std.process.argsAlloc(std.heap.page_allocator);
+    std.debug.print("{s} {}", .{args, args.len});
 }
