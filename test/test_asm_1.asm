405855e5d6156eae1c3c2d705877bb1e4da2e7c3
MeParthDegama <hello@parthka.com>
2023-07-12 09:37:17 +0530

    add test file

--- test/test_asm_1.asm
+++ test/test_asm_1.asm
@@ -1,4 +1,18 @@
 %bits 64
 
+%text
 
+$start:
+  mov rax, 1       
+  mov rdi, 1        ;   STDOUT_FILENO,
+  mov rsi, $msg      ;   "Hello, world!\n",
+  mov rdx, 14   ;   sizeof("Hello, world!\n")
+  syscall           ; );
 
+  mov rax, 60       ; exit(
+  mov rdi, 0        ;   EXIT_SUCCESS
+  syscall           ; );
+
+%data
+
+  $msg: db "Hello, world!", 10
